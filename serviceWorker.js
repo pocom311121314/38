cbf035d2985551df564e813c3879203075b2ce84
pocom311121314 <pocom311121314@gmail.com>
2023-09-02 21:22:22 +0530

    wd

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,19 @@
+const dc = "dcshopv3"
+const assets = [
+  "/",
+  "/index.html",
+  "/static/style.css",
+  "/static/data.js",
+  
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
 self.addEventListener("fetch", fetchEvent => {
     fetchEvent.respondWith(
       caches.match(fetchEvent.request).then(res => {
