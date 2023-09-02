57a2095d88197465a4e0153f6807d8686f208d47
pocom311121314 <pocom311121314@gmail.com>
2023-09-02 21:04:51 +0530

    fe

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-      caches.match(event.request).then(function(response) {
-          return response || fetch(event.request);
-      })
-  );
-});
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+  });
