41e15dc8404f9ec4b333a56d43f933e1fbd7d3d1
pocom311121314 <pocom311121314@gmail.com>
2023-09-02 20:53:12 +0530

    brbe

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-self.addEventListener('fetch', function(event) {
+self.beforeinstallprompt('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
             return response || fetch(event.request);
